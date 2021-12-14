daaae0813a55a4711ff762a27c9b7835f56cf5e5
VijayAdam <765139628@qq.com>
2021-12-14 15:27:11 +0800

    我又修改了

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ console.log("我来自dev1 改了些内容");
 console.log(1);
 console.log("我来自master2 改了点内容")
 console.log("我来自vijay12 顺便该店内容")
-console.log("我来自vijay3 顺便该店内容")
-console.log("我来自vijay22 顺便该店内容")
+console.log("我来自vijay3asd 顺便该店内容")
+console.log("我来自vijay22asd 顺便该店内容")
 console.log("我来自dev1 改了些内容");
