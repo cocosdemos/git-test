0edb47d15bd7646b0a98db688a6172be13ef2263
VijayAdam <765139628@qq.com>
2021-12-14 15:25:13 +0800

    fix by master

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log("我来自dev1 改了些内容");
 console.log(1);
 console.log("我来自master2 改了点内容")
-console.log("我来自vijay1 顺便该店内容")
-console.log("我来自master33 改了点内容")
-console.log("我来自vijay 顺便该店内容")
+console.log("我来自vijay11 顺便该店内容")
+console.log("我来自master332 改了点内容")
+console.log("我来自vijay3 顺便该店内容")
 console.log("我来自vijay2 顺便该店内容")
 console.log("我来自dev1 改了些内容");
