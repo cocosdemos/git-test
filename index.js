1ff514b58b8e737e5df2843a81fca909b753baac
VijayAdam <765139628@qq.com>
2021-12-14 15:25:41 +0800

    fix by vijay-feat
    我又修改了

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ console.log("我来自vijay11 顺便该店内容")
 console.log("我来自master是332 改了点内容")
 console.log("我来自vijay3 顺便该店内容")
 console.log("我来自vijay2 顺便该店内容")
+console.log("我来自vijay12 顺便该店内容")
+console.log("我来自vijay3asd 顺便该店内容")
+console.log("我来自vijay22asd 顺便该店内容")
 console.log("我来自dev1 改了些内容");
