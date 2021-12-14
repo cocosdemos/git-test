9fdcefc457b53b5053f8000d910836be5d25a3bc
VijayAdam <765139628@qq.com>
2021-12-14 15:27:49 +0800

    我也修改了

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("我来自dev1 改了些内容");
 console.log(1);
 console.log("我来自master2 改了点内容")
 console.log("我来自vijay11 顺便该店内容")
-console.log("我来自master332 改了点内容")
+console.log("我来自master是332 改了点内容")
 console.log("我来自vijay3 顺便该店内容")
 console.log("我来自vijay2 顺便该店内容")
 console.log("我来自dev1 改了些内容");
